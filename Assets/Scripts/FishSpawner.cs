82bd19a532a81bc6d6de494d37de3a3bca36332c
Sabien <ambid17@gmail.com>
2022-09-20 17:17:40 -0500

    add fish spawner

---
+++ Assets/Scripts/FishSpawner.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSpawner : MonoBehaviour
+{
+    public List<GameObject> fishPrefabs;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
